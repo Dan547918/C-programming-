f1689d0ad858122276b3f397a3b07f1f887fa669
Dan547918 <mugambiduncan45@gmail.com>
2025-09-25 10:00:28 +0300

    Add files via upload

---
+++ Loan.c
@@ -0,0 +1,31 @@
+/*
+Name:Duncan mugambi
+Reg no. 26547
+Date: 12th sept
+Description: Loan eligibility
+*/
+
+#include <stdio.h>
+    int main(){
+        int age;
+        float income;
+        
+       //user inputs
+       
+     printf("Please enter your age \n");
+     scanf("%d", & age);
+     
+     printf("please enter your income \n");
+     scanf("%f" , & income);
+         
+     //Eligibility check
+      if(age>=21 && income >=21000){
+          printf("Congratulations you qualify for the loan");}
+           else{
+               printf("Unfortunately we are unable to offer you a loan at this time . \n ");
+           }
+           
+           return 0;
+    }
+               
+               
