7b1d279cf177f8c1d01df6e73a1ddea6b9a3bc47
Dan547918 <mugambiduncan45@gmail.com>
2025-10-09 23:30:13 +0300

    Add files via upload

---
+++ For_loop.c
@@ -0,0 +1,15 @@
+/*
+Name : Duncan Mugambi Gitonga
+Reg no: CT101/G/26547/25
+Description: using while loop to guess secret number
+*/
+#include <stdio.h>
+
+int main() {
+//variable declaration 
+    int i;
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+    return 0;
+}
