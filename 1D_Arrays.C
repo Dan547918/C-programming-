271d847105aa2b9a152f37bd8271fffb4ffb7252
Dan547918 <mugambiduncan45@gmail.com>
2025-10-30 23:20:24 +0300

    Add files via upload

---
+++ 1D_Arrays.C
@@ -0,0 +1,24 @@
+/*
+Name: Duncan Mugambi Gitonga
+Reg No:CT101/G/26547/25
+Descriprion: Hotel management system using arrays
+*/
+# include <stdio.h>
+
+int main(){
+	int i,sum=0,avarage;
+	// declare and initialize
+	int revenue [7]={200,500,400,700,900,300,600};
+	
+	//print values
+	 for(i=0;i<7;i++){
+	     printf("%d\n",revenue[i]);
+	     sum=sum+revenue[i];
+	     avarage=sum/7;
+		 };
+		 
+		printf("sum=%d\n",sum);
+		printf("avarage=%d",avarage);
+	 	
+	return 0;
+}
